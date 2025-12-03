baf936db847c7b8869aeec080b0ffa70a1188334
michaelpisarri-git <michaelpisarri@gmail.com>
2025-12-02 23:24:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       // This ensures process.env.API_KEY is available in the browser code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
     }
   }
 })
